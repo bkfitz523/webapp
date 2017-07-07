12598af768655cf8fe2b561cc3c376949422231e
bkfitz523 <bkfitz523@gmail.com>
2017-07-07 19:19:20 -0400

    Updates to Database and Admin

--- collection/admin.py
+++ collection/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from collection.models import Pokemon
 
 # Register your models here.
+
+class PokemonAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Pokemon, PokemonAdmin)
--- collection/models.py
+++ collection/models.py
@@ -3,18 +3,25 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
 class Pokemon(models.Model):
-    id = models.IntegerField(max_length=4)
+    id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=50)
     type_1 = models.CharField(max_length=20)
-    type_2 = models.CharField(max_length=20)
+    type_2 = models.CharField(max_length=20, blank=True)
     region = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.name
+
+'''
 class Trainer(models.Model):
     id = models.IntegerField(max_length=7)
     name = models.CharField(max_length=50)
     hometown = models.CharField(max_length=127)
     region = models.CharField(max_length=50)
+
 
 class Team(models.Model):
     id = models.IntegerField(max_length=8)
@@ -25,3 +32,4 @@ class Team(models.Model):
     pokemon_04 = models.CharField(max_length=50)
     pokemon_05 = models.CharField(max_length=50)
     pokemon_06 = models.CharField(max_length=50)
+'''
