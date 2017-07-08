14ca268b2e38ccdde3dd1f95c48ba99af1306fd7
bkfitz523 <bkfitz523@gmail.com>
2017-07-07 21:48:47 -0400

    Add data and update admin layout

--- collection/admin.py
+++ collection/admin.py
@@ -3,6 +3,9 @@ from collection.models import Pokemon
 
 # Register your models here.
 
+
 class PokemonAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('id', 'name', 'type_1', 'type_2', 'region')
+    list_filter = ('type_1', 'type_2', 'region')
+    ordering = ('id',)
 admin.site.register(Pokemon, PokemonAdmin)
